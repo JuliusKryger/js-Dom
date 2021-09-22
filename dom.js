327159e39c51d69d9e98491c6576361fdcb6ebb0
Julius Madsen <2330051117@protonmail.com>
2021-09-22 18:49:38 +0200

    intial commit

---
+++ dom.js
@@ -0,0 +1,21 @@
+function colorButton() {
+    let div1 = document.getElementById("1")
+    let div2 = document.getElementById("3")
+    let div3 = document.getElementById("2")
+    
+    div1.style.backgroundColor = "red"
+    div2.style.backgroundColor = "blue"
+    div3.style.backgroundColor = "yellow"
+}
+
+function idOfTheDiv1() {
+    
+    console.log("Hi from div 1")
+
+}
+
+function idOfTheDiv2() {
+    
+    console.log("Hi from div 2")
+
+}
