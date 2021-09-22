ea0436d4140aaa51074829b046b15d7652436e2e
Julius Madsen <2330051117@protonmail.com>
2021-09-22 19:07:08 +0200

    commit

--- dom.js
+++ dom.js
@@ -1,21 +1,18 @@
 function colorButton() {
-    let div1 = document.getElementById("1")
-    let div2 = document.getElementById("3")
-    let div3 = document.getElementById("2")
-    
-    div1.style.backgroundColor = "red"
-    div2.style.backgroundColor = "blue"
-    div3.style.backgroundColor = "yellow"
+    let div1 = document.getElementById("1"); let div2 = document.getElementById("3"); let div3 = document.getElementById("2")
+    div1.style.backgroundColor = "red"; div2.style.backgroundColor = "blue"; div3.style.backgroundColor = "yellow"
 }
 
 function idOfTheDiv1() {
-    
     console.log("Hi from div 1")
-
 }
 
 function idOfTheDiv2() {
-    
     console.log("Hi from div 2")
-
 }
+
+let outerDiv = document.getElementById("outer")
+function outerCall() { outerDiv.addEventListener('click', event => {
+    event.preventDefault(); document.getElementById("outputtext").innerHTML = event.target.getAttribute('id')
+    console.log(event.target.getAttribute('id'))
+})}
